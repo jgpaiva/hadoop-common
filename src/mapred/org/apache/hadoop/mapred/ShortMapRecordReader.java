9953336369dbec66e9028ca36fb0594395f8b884
Chathuri <chathuri@ubuntu.(none)>
2014-07-03 23:12:16 +0100

    Changed the location of preprocessing code

--- src/mapred/org/apache/hadoop/mapred/ShortMapRecordReader.java
+++ src/mapred/org/apache/hadoop/mapred/ShortMapRecordReader.java
@@ -20,7 +20,8 @@ import org.apache.hadoop.io.compress.Decompressor;
 import org.apache.hadoop.io.compress.SplittableCompressionCodec;
 import org.mortbay.log.Log;
 
-public class ShortMapRecordReader implements RecordReader<LongWritable, Text> {
+public class ShortMapRecordReader implements RecordReader<LongWritable, Text>
+{
 
 	private static final org.apache.commons.logging.Log LOG = LogFactory
 			.getLog(ShortMapRecordReader.class.getName());
@@ -55,23 +56,28 @@ public class ShortMapRecordReader implements RecordReader<LongWritable, Text> {
 	 * @deprecated Use {@link org.apache.hadoop.util.LineReader} instead.
 	 */
 	@Deprecated
-	public static class LineReader extends org.apache.hadoop.util.LineReader {
-		LineReader(InputStream in) {
+	public static class LineReader extends org.apache.hadoop.util.LineReader
+	{
+		LineReader(InputStream in)
+		{
 			super(in);
 		}
 
-		LineReader(InputStream in, int bufferSize) {
+		LineReader(InputStream in, int bufferSize)
+		{
 			super(in, bufferSize);
 		}
 
 		public LineReader(InputStream in, Configuration conf)
-				throws IOException {
+				throws IOException
+		{
 			super(in, conf);
 		}
 	}
 
 	public ShortMapRecordReader(Configuration job, ShortMapFileSplit split)
-			throws IOException {
+			throws IOException
+	{
 		this.currentRowGroupIndex = 0;
 		this.job = job;
 		this.split = split;
@@ -87,7 +93,8 @@ public class ShortMapRecordReader implements RecordReader<LongWritable, Text> {
 	 * 
 	 * @throws IOException
 	 */
-	private void openRowGroup() throws IOException {
+	private void openRowGroup() throws IOException
+	{
 		LOG.info("inside create row group");
 		currentRowGroupIndex = split.getRowGroupId();
 		String rowGroupName = split.getRowGroupName();
@@ -109,10 +116,12 @@ public class ShortMapRecordReader implements RecordReader<LongWritable, Text> {
 		String indexNode = split.getRowGroupName().split(
 				Constants.SPECIFIC_PREFIX)[0];
 		String status = "-1";// 0;0;0
-		try {
+		try
+		{
 			status = MapTask.relevantRowGroup(firstFile.getName(), indexNode,
 					job);
-		} catch (Exception ex) {
+		} catch (Exception ex)
+		{
 			ex.printStackTrace();
 		}
 		ArrayList<Long> offsetList = new ArrayList<Long>();
@@ -122,19 +131,23 @@ public class ShortMapRecordReader implements RecordReader<LongWritable, Text> {
 		String[] valSet = status.split(";");
 		statusId = Integer.parseInt(valSet[0]);
 
-		if (statusId != -1) {
-			if (statusId > 0) {
+		if (statusId != -1)
+		{
+			if (statusId > 0)
+			{
 				for (int i = 1; i < valSet.length; i++)
 					offsetList.add(Long.parseLong(valSet[i]));
-			} else {
-				for (int i = 1; i < valSet.length; i++) {
+			} else
+			{
+				for (int i = 1; i < valSet.length; i++)
+				{
 					offsetList.add(0L);// All files should be started from
 										// started
 										// position
 				}
 			}
 
-			LOG.info("other offset rec count " + offsetList.size());
+			// LOG.info("other offset rec count " + offsetList.size());
 
 			FileSystem fs = firstFile.getFileSystem(job);
 			this.maxLineLength = job.getInt(
@@ -146,7 +159,8 @@ public class ShortMapRecordReader implements RecordReader<LongWritable, Text> {
 			codec = compressionCodecs.getCodec(firstFile);
 			FSDataInputStream fileIn = fs.open(firstFile);
 			int i = 0;
-			for (Path path : pathsToRelavantAttr) {
+			for (Path path : pathsToRelavantAttr)
+			{
 				array2inputStreams.add(i, fs.open(path));
 				startN.add(i, new Long(0));
 				CompressionCodecFactory compressionCodecsTmp = new CompressionCodecFactory(
@@ -155,28 +169,33 @@ public class ShortMapRecordReader implements RecordReader<LongWritable, Text> {
 				i++;
 			}
 
-			if (isCompressedInput()) {
+			if (isCompressedInput())
+			{
 				decompressor = CodecPool.getDecompressor(codec);
-				if (!(codec instanceof SplittableCompressionCodec)) {
+				if (!(codec instanceof SplittableCompressionCodec))
+				{
 					CompressionInputStream is = codec.createInputStream(fileIn,
 							decompressor);
-					if (statusId != 0) {
+					if (statusId != 0)
+					{
 						is.skip(statusId);
 					}
 					in = new LineReader(is, job);
 					filePosition = fileIn;
 
 					i = 0;
-					for (FSDataInputStream fileInAttr : array2inputStreams) {
+					for (FSDataInputStream fileInAttr : array2inputStreams)
+					{
 						CompressionCodec codecTmp = codecN.get(i);
 						Decompressor decompressorTmp = CodecPool
 								.getDecompressor(codecTmp);
 						CompressionInputStream isN = codecTmp
 								.createInputStream(fileInAttr, decompressorTmp);
-						if (offsetList.get(i) != 0) {// Offset to be correct,
-														// users should specify
-														// the attribute numbers
-														// in ascending order
+						if (offsetList.get(i) != 0)
+						{// Offset to be correct,
+							// users should specify
+							// the attribute numbers
+							// in ascending order
 							isN.skip(offsetList.get(i));
 						}
 						inN.add(i, new LineReader(isN, job));
@@ -184,13 +203,15 @@ public class ShortMapRecordReader implements RecordReader<LongWritable, Text> {
 						i++;
 					}
 				}
-			} else {
+			} else
+			{
 				fileIn.seek(statusId);
 				in = new LineReader(fileIn, job);
 				filePosition = fileIn;
 
 				i = 0;
-				for (FSDataInputStream fileInN : array2inputStreams) {
+				for (FSDataInputStream fileInN : array2inputStreams)
+				{
 					fileInN.seek(offsetList.get(i));
 					inN.add(i, new LineReader(fileInN, job));
 					filePositionN.add(i, fileInN);
@@ -200,11 +221,13 @@ public class ShortMapRecordReader implements RecordReader<LongWritable, Text> {
 			// If this is not the first split, we always throw away first record
 			// because we always (except the last split) read one extra line in
 			// next() method.
-			if (start != 0) {
+			if (start != 0)
+			{
 				start += in.readLine(new Text(), 0, maxBytesToConsume(start));
 
 				i = 0;
-				for (LineReader reader : inN) {
+				for (LineReader reader : inN)
+				{
 					long startn = startN.remove(i).longValue();
 					startn += reader.readLine(new Text(), 0,
 							maxBytesToConsume(startn));
@@ -214,7 +237,8 @@ public class ShortMapRecordReader implements RecordReader<LongWritable, Text> {
 			}
 			this.pos = start;
 			i = 0;
-			for (Long startn : startN) {
+			for (Long startn : startN)
+			{
 				posN.add(i, startn);
 				i++;
 			}
@@ -224,58 +248,67 @@ public class ShortMapRecordReader implements RecordReader<LongWritable, Text> {
 
 	/**
 	 * @param rowGroupName
-	 *            Return the list of relevant paths and the first file path
+	 *            Return the list of relevant paths. The first column should
+	 *            always be specified by the user
 	 */
-	private ArrayList<Path> getRelavantAttrPaths(Path file, String rowGroupName) {
+	private ArrayList<Path> getRelavantAttrPaths(Path file, String rowGroupName)
+	{
 		// TODO Auto-generated method stub
 		ArrayList<Path> relevantPaths = new ArrayList<Path>();
 		String fileName = file.getName();
 		String firstFilePart = Constants.COLUMN_PREFIX + 0 + "_" + rowGroupName;
 		String relevantAttrStr = job.get(Constants.RELEVANT_ATTR_CONFIG);
 
-		LOG.info("Relavant config is " + relevantAttrStr);
-
 		String[] relevantAttrs = relevantAttrStr.split(",");
 		Path filePath = file.getParent();
 
-		for (String attr : relevantAttrs) {
+		for (String attr : relevantAttrs)
+		{
 			String newFilePart = Constants.COLUMN_PREFIX + attr + "_"
 					+ rowGroupName;
 			String newFileName = fileName.replace(firstFilePart, newFilePart);
 
-			System.out.println("Getting relavant path for " + newFileName);
+			// System.out.println("Getting relavant path for " + newFileName);
 			Path path = new Path(filePath, newFileName);
-			if (attr.equals("0")) {
+			if (attr.equals("0"))
+			{
 				Log.info("Is in 0 th pos");
 				relevantPaths.add(0, path);
-			} else {
+			} else
+			{
 				relevantPaths.add(path);
 			}
 		}
 		return relevantPaths;
 	}
 
-	private boolean isCompressedInput() {
+	private boolean isCompressedInput()
+	{
 		return (codec != null);
 	}
 
-	private int maxBytesToConsume(long pos) {
+	private int maxBytesToConsume(long pos)
+	{
 		return isCompressedInput() ? Integer.MAX_VALUE : (int) Math.min(
 				Integer.MAX_VALUE, end - pos);
 	}
 
-	private long getFilePosition() throws IOException {
+	private long getFilePosition() throws IOException
+	{
 		long retVal;
-		if (isCompressedInput() && null != filePosition) {
+		if (isCompressedInput() && null != filePosition)
+		{
 			retVal = filePosition.getPos();
-		} else {
+		} else
+		{
 			retVal = pos;
 		}
 		return retVal;
 	}
 
 	public ShortMapRecordReader(InputStream in, long offset, long endOffset,
-			int maxLineLength) {
+			int maxLineLength)
+	{
 		this.maxLineLength = maxLineLength;
 		this.in = new LineReader(in);
 		this.start = offset;
@@ -285,7 +318,8 @@ public class ShortMapRecordReader implements RecordReader<LongWritable, Text> {
 	}
 
 	public ShortMapRecordReader(InputStream in, long offset, long endOffset,
-			Configuration job) throws IOException {
+			Configuration job) throws IOException
+	{
 		this.maxLineLength = job.getInt("mapred.linerecordreader.maxlength",
 				Integer.MAX_VALUE);
 		this.in = new LineReader(in, job);
@@ -296,17 +330,24 @@ public class ShortMapRecordReader implements RecordReader<LongWritable, Text> {
 	}
 
 	@Override
-	public boolean next(LongWritable key, Text value) throws IOException {
-		while (getFilePosition() <= end) {
-			if (stop()) {// we should process only if there is a record to
-							// process
+	/**
+	 * Edited original behavior to combine lines from selected attributes and pass that to the map function
+	 */
+	public boolean next(LongWritable key, Text value) throws IOException
+	{
+		while (getFilePosition() <= end)
+		{
+			if (stop())
+			{// we should process only if there is a record to
+				// process
 				return false;
 			}
 			key.set(pos);
 			int newSize = in.readLine(value, maxLineLength,
 					Math.max(maxBytesToConsume(pos), maxLineLength));
 			// Text accumulator = new Text(value.toString());
-			if (newSize == 0) {
+			if (newSize == 0)
+			{
 				return false;
 			}
 			int i = 0;
@@ -314,17 +355,17 @@ public class ShortMapRecordReader implements RecordReader<LongWritable, Text> {
 			int columnSize = 0;
 			Text columnLine;
 
-			System.out.println("combimed file count is " + inN.size());
-
-			for (LineReader in1 : inN) {
+			for (LineReader in1 : inN)
+			{
 				columnLine = new Text();
 				posLength = posN.get(i);
 				columnSize = in1.readLine(columnLine, maxLineLength,
 						Math.max(maxBytesToConsume(posLength), maxLineLength));
 
-				LOG.info("Line to add is " + columnLine);
+				// LOG.info("Line to add is " + columnLine);
 				value.set(value.toString() + ";$;#;" + columnLine.toString());
-				if (columnSize != 0) {
+				if (columnSize != 0)
+				{
 					posLength += columnSize;
 					posN.remove(i);
 					posN.add(i, new Long(posLength));
@@ -332,65 +373,77 @@ public class ShortMapRecordReader implements RecordReader<LongWritable, Text> {
 				i++;
 
 				pos += newSize;
-
 			}
-			if (newSize < maxLineLength) {
-				LOG.info("accumulator str is " + value.toString());
+			if (newSize < maxLineLength)
+			{
+				// LOG.info("accumulator str is " + value.toString());
 				return true;
 			}
 			// line too long. try again
 			LOG.info("Skipped line of size " + newSize + " at pos "
 					+ (pos - newSize));
 		}
-		// close();
 		return false;
 	}
 
 	/**
 	 * @return
 	 */
-	private boolean stop() {
+	private boolean stop()
+	{
 		// TODO Auto-generated method stub
 		return statusId == -1 ? true : false;
 	}
 
 	@Override
-	public LongWritable createKey() {
+	public LongWritable createKey()
+	{
 		// TODO Auto-generated method stub
 		return new LongWritable();
 	}
 
 	@Override
-	public Text createValue() {
+	public Text createValue()
+	{
 		// TODO Auto-generated method stub
 		return new Text();
 	}
 
 	@Override
-	public long getPos() throws IOException {
+	public long getPos() throws IOException
+	{
 		// TODO Auto-generated method stub
 		return pos;
 	}
 
 	@Override
-	public void close() throws IOException {
-		try {
+	public void close() throws IOException
+	{
+		try
+		{
 			if (in != null)
 				in.close();
 
-			for (LineReader in : inN) {
-				try {
-					if (in != null) {
+			for (LineReader in : inN)
+			{
+				try
+				{
+					if (in != null)
+					{
 						in.close();
 					}
-				} finally {
-					if (decompressor != null) {
+				} finally
+				{
+					if (decompressor != null)
+					{
 						CodecPool.returnDecompressor(decompressor);
 					}
 				}
 			}
-		} finally {
-			if (decompressor != null) {
+		} finally
+		{
+			if (decompressor != null)
+			{
 				CodecPool.returnDecompressor(decompressor);
 			}
 		}
@@ -398,10 +451,13 @@ public class ShortMapRecordReader implements RecordReader<LongWritable, Text> {
 	}
 
 	@Override
-	public float getProgress() throws IOException {
-		if (start == end) {
+	public float getProgress() throws IOException
+	{
+		if (start == end)
+		{
 			return 0.0f;
-		} else {
+		} else
+		{
 			return Math.min(1.0f, (getFilePosition() - start)
 					/ (float) (end - start));
 		}
